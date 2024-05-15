3b4abc11e53f2ce533fb0c3f6eded1d159d5a1cb
fabiiianac15 <fjacostaa@ufpso.edu.co>
2024-05-15 12:30:42 -0500

    Cositas

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+let msg = ref('Soy un tooltip')
+let checked = ref(false)
+
+let data = ["Hola", "Mundo", "Bonita"]
+let filtered = data.filter((word) => word.includes('o'))
+</script>
+
+<template>
+    <input class="border" :class="{ 'border-red-500': checked }" v-model="msg" />
+    <div :title="msg">
+        {{ msg }}
+    </div>
+    <label for="check">Seleccionado {{ checked }}</label>
+    <input type="checkbox" v-model="checked">
+
+    <ul>
+        <li v-for="(text,index) in filtered" :key="index">
+            {{ text }}
+        </li>
+    </ul>
+</template>
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+const task = ref([])
+const inputTask = ref ('')
+function addTask (){
+    task.value.push(inputTask.value)
+    console.log(task)
+}
+</script>
+
+<template>
+    <div class=" py-8 px-10 max-w-96 bg-slate-300 rounded shadow mx-auto mt-10 ">
+        <h1 class=" text-center text-lg ">Lista de Tareas</h1>
+        <div class="mt-4 mb-2">
+            <input v-model="inputTask" class=" w-4/5 " />
+            <button @click="addTask" class=" w-1/5 bg-green-600 text-white font-bold">+</button>
+        </div>
+        <div v-for="(task,index) in task" :key="index" class=" bg-white/60 px-2 mb-1">
+            {{ task }}
+        </div>
+    </div>
+</template>
